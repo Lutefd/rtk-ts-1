dd0703cd3f63fca3546ddafb1186599ede19e1e1
luis dourado <lfaddourado@gmail.com>
2023-04-11 10:07:01 -0300

    feat: change reset functionality and add add by amount

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -3,7 +3,9 @@ import {
   increment,
   decrement,
   reset,
+  incrementByAmount,
 } from '../store/services/Counter/counterSlice';
+import { useState } from 'react';
 const Counter = () => {
   interface RootState {
     counter: {
@@ -13,13 +15,36 @@ const Counter = () => {
 
   const count = useSelector((state: RootState) => state.counter.count);
   const dispatch = useDispatch();
+  const [amount, setAmount] = useState(0);
+
+  const handleAmount = (e: string) => {
+    setAmount(parseInt(e));
+  };
+
+  const resetEverything = () => {
+    dispatch(reset());
+    setAmount(0);
+  };
+
   return (
     <section className="card">
       <h1>{count}</h1>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
-        <button onClick={() => dispatch(reset())}>Reset</button>
+        <button onClick={resetEverything}>Resetar</button>
+      </div>
+      <div>
+        <label htmlFor="amount">Valor a ser adicionado</label>
+        <input
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(e) => handleAmount(e.target.value)}
+        />
+        <button onClick={() => dispatch(incrementByAmount(amount))}>
+          Adicionar valor
+        </button>
       </div>
     </section>
   );
